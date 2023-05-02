aefa943e4ea8ce7462d7409ee93dc8ebebcc6c3c
lr.lguirre <lr.lguirre@mmdc.mcl.edu.ph>
2023-05-03 02:06:54 +0800

    testing

--- src/main/java/moit103s1102g7/version4/exit.java
+++ src/main/java/moit103s1102g7/version4/exit.java
@@ -2,6 +2,6 @@ package moit103s1102g7.version4;
 
 public class exit {
     public static void Exit() {
-        System.out.println("Thank your for using MotorPH HRIS!");
+        System.out.println("Thank your for using MotorPH!"); 
     }
 }
